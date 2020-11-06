f7578128043255087d8fbe5dc40ac5de49a0238d
MinaLbc <48332228+MinaLbc@users.noreply.github.com>
2020-11-06 16:59:08 +0100

    Scripts

---
+++ Ink_proto/Assets/Script/Child.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Child : MonoBehaviour
+{
+	public Transform target;
+	public float speed;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	Vector3 pos = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        GetComponent<Rigidbody>().MovePosition(pos);		
+        
+    }
+}
